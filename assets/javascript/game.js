f63fa0a9c42aa0b7f2110a94f101d6ba70ea0364
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-02-28 05:22:07 -0500

    Add resetGame, onkeyup listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,26 @@
 // starting variables
-const gameWordList = ["live", 
-"aromatic", 
-"heavenly", 
-"whole", 
-"force", 
-"dogs", 
-"erratic", 
-"abnormal", 
-"magical", 
-"swift", 
-"psychotic", 
-"pointless", 
-"reaction", 
-"narrow", 
-"channel", 
-"near", 
-"mute", 
-"flagrant", 
-"brake", 
-"line"];
+const gameWordList = ["live",
+    "aromatic",
+    "heavenly",
+    "whole",
+    "force",
+    "dogs",
+    "erratic",
+    "abnormal",
+    "magical",
+    "swift",
+    "psychotic",
+    "pointless",
+    "reaction",
+    "narrow",
+    "channel",
+    "near",
+    "mute",
+    "flagrant",
+    "brake",
+    "line"];
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 var playerGuess = '';
 var playerGuesses = "";
@@ -36,7 +38,7 @@ spanGuesses = document.getElementById("displayGuesses");
 
 // Select a word from the given list
 function randomWord(wordList) {
-    var index = Math.floor(Math.random()*wordList.length);
+    var index = Math.floor(Math.random() * wordList.length);
     return wordList[index];
 }
 
@@ -60,12 +62,12 @@ function guessesToString(guesses) {
     for (var k = 0; k < guesses.length; k++) {
         guessString += guesses[k];
         // don't add a terminal comma
-        if (k < guesses.length-1) {
+        if (k < guesses.length - 1) {
             guessString += ', '
         }
     }
     return guessString.toUpperCase();
- 
+    
 }
 
 // To be called after a player makes a guess
@@ -77,5 +79,30 @@ function updateGameBoard() {
     // spanGuessedWord.innerHTML = "guessed word goes here"; // wordToString(word, guesses);
     spanGuesses.innerHTML = guessesToString(playerGuesses);
     // spanGuesses.innerHTML = "guesses go here"; // guessesToString(guesses);
-
+    
 }
+
+function resetGame() {
+    currentWord = randomWord(gameWordList);
+    playerGuesses = "";
+    guessesLeft = 9;
+}
+
+
+document.onkeyup = function (event) {
+    
+    // Determines which key was pressed.
+    var userGuess = event.key;
+    
+    if ((alphabet.indexOf(userGuess) === -1) || (playerGuesses.indexOf(userGuess) !== -1)) { return 0; }
+    
+    console.log(userGuess);
+    playerGuesses = playerGuesses + userGuess;
+    updateGameBoard();
+    
+    // TODO: detect win and loss conditions, consider separating good and bad guesses
+    // consider making intermediate string of filled in guesses to detect win
+};
+
+resetGame();
+updateGameBoard();

dacac91f8d5ef7b8a87e04e444d333cb1ef341d1
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-03-04 01:34:38 -0500

    Test imageflipper

---
+++ assets/javascript/imageflipper.js
@@ -0,0 +1,25 @@
+var guessesLeft = 9;
+
+hangmanImage = document.getElementById("imageCycler");
+
+cycleButton = document.getElementById("buttonCycle");
+resetButton = document.getElementById("buttonReset");
+
+
+
+cycleButton.onclick = function () {
+    if (guessesLeft >= 0) {
+        hangmanImage.src = "assets/images/hangman" + --guessesLeft + ".png";
+
+    } else {
+        guessesLeft = 9;
+    }
+
+};
+
+resetButton.onclick = function () {
+    hangmanImage.src = "assets/images/hangman9.png";
+    guessesLeft = 9;
+
+};
+
